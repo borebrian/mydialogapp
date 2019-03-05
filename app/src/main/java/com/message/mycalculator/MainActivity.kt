8b166b253967f136e21357dd8a1f8f9062ed51a2
borebrian <borebrian77@gmail.com>
2019-03-05 17:36:14 +0300

    Initial commit

---
+++ app/src/main/java/com/message/mycalculator/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.message.mycalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        one.setOnClickListener(){
+
+        }
+        two.setOnClickListener(){
+
+        }
+        three.setOnClickListener(){
+
+        }
+        four.setOnClickListener(){
+
+        }
+        five.setOnClickListener(){
+
+        }
+        six.setOnClickListener(){
+
+        }
+        seven.setOnClickListener(){
+
+        }
+        eight.setOnClickListener(){
+
+        }
+        nine.setOnClickListener(){
+
+        }
+    }
+
+
+}

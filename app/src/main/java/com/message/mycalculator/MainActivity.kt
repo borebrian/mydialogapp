ae5cbcb3f0600d667469a60c29b7fd38f545b4b3
borebrian <borebrian77@gmail.com>
2019-03-06 17:38:22 +0300

    My calc

--- app/src/main/java/com/message/mycalculator/MainActivity.kt
+++ app/src/main/java/com/message/mycalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.message.mycalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,34 +11,52 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        one.setOnClickListener(){
+        one.setOnClickListener() {
+           val snack = Snackbar.make(it,"1",Snackbar.LENGTH_SHORT)
+            snack.show()
 
         }
-        two.setOnClickListener(){
 
-        }
-        three.setOnClickListener(){
 
-        }
-        four.setOnClickListener(){
+            two.setOnClickListener() {
+                val build=AlertDialog.Builder(this)
 
-        }
-        five.setOnClickListener(){
+            }
+            three.setOnClickListener() {
 
-        }
-        six.setOnClickListener(){
+            }
+            four.setOnClickListener() {
 
-        }
-        seven.setOnClickListener(){
+            }
+            five.setOnClickListener() {
 
-        }
-        eight.setOnClickListener(){
+            }
+            six.setOnClickListener() {
 
-        }
-        nine.setOnClickListener(){
+            }
+            seven.setOnClickListener() {
 
-        }
+            }
+            eight.setOnClickListener() {
+
+            }
+            nine.setOnClickListener() {
+
+            }
+            times.setOnClickListener() {
+
+            }
+            devide.setOnClickListener() {
+
+            }
+            equals.setOnClickListener() {
+
+            }
+            results.setOnClickListener() {
+
+            }
+
+
+
     }
-
-
 }
